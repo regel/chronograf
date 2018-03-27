be2d51cd7f63fc8a66a75f06a79033b1e6bb1bad
Vianney Bajart <vianney@redmintnetwork.fr>
2018-03-27 10:44:51 +0200

    loudml: add proxy for /loudml/api

--- ui/webpack/dev.config.js
+++ ui/webpack/dev.config.js
@@ -226,6 +226,13 @@ module.exports = {
         target: 'http://localhost:8888',
         secure: false,
       },
+      '/loudml/api': {
+        target: 'http://localhost:8077',
+        secure: false,
+        pathRewrite: {
+          '^/loudml/api': '/',
+        },
+      },
     },
   },
 }
