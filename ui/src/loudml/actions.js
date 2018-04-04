92f6ccadac9890e72007bc960e52d370788bf048
Vianney Bajart <vianney@redmintnetwork.fr>
2018-04-04 17:16:51 +0200

    loudml: implement model deletion

--- ui/src/loudml/actions.js
+++ ui/src/loudml/actions.js
@@ -1,5 +1,11 @@
-import {getModels as getModelsAJAX} from 'src/loudml/apis'
+import {
+  deleteModel,
+  getModels as getModelsAJAX,
+} from 'src/loudml/apis'
+import {notify} from 'shared/actions/notifications'
 import {errorThrown} from 'shared/actions/errors'
+import {HTTP_NOT_FOUND} from 'shared/constants'
+import {notifyServerError} from 'shared/copy/notifications'
 
 export const addModel = model => ({
   type: 'LOUDML_MODEL_ADDED',
@@ -27,5 +33,24 @@ export const loadModelsAsync = () => async dispatch => {
     dispatch(loadModels(data))
   } catch (error) {
     dispatch(errorThrown(error))
+  }
+}
+export const removeAndLoadModels = modelName => async dispatch => {
+  try {
+    try {
+      await deleteModel(modelName)
+    } catch (err) {
+      // A 404 means that either a concurrent write occurred or the model
+      // passed to this action creator doesn't exist (or is undefined)
+      if (err.status !== HTTP_NOT_FOUND) {
+        // eslint-disable-line no-magic-numbers
+        throw err
+      }
+    }
+
+    const {data: newModels} = await getModelsAJAX()
+    dispatch(loadModels(newModels))
+  } catch (err) {
+    dispatch(notify(notifyServerError()))
   }
 }
