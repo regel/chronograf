5c9ce9221c1a8e02f1c569819984be86db07629c
Samuel Bettevy <samuel.bettevy@gmail.com>
2018-07-04 18:53:52 +0200

    1-ClickML

--- ui/src/loudml/components/ModelStatus.js
+++ ui/src/loudml/components/ModelStatus.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import {PropTypes} from 'prop-types'
 
+import ProgressCode from 'src/loudml/components/ProgressCode'
+
+import 'src/loudml/styles/status.css'
+
 const ModelStatus = ({
     model,
     jobs,
 }) => {
     return (
-        <div>
+        <div className="status">
             {model.settings.run ?
                 <code>Running</code>
                 : null
             }
-            {model.training ?
+            {model.training
+                && model.training.state==='running'
+                && model.training.progress !== undefined ? (
+                <ProgressCode
+                    max={model.training.progress.max_evals}
+                    value={model.training.progress.eval}
+                    label={`Train ${model.training.state}`} />
+                ) : null}
+            {model.training && model.training.state !== 'running' ? (
                 <code>
                     Train&nbsp;{model.training.state}
                 </code>
-                : null
-            }
-            {jobs.map(job => (
+            ) : null}
+            {jobs
+                .filter(job => job.type==='forecast')
+                .map(job => (
                 <code key={job.id}>
                     {job.type}&nbsp;{job.state}
                 </code>)
@@ -25,6 +38,22 @@ const ModelStatus = ({
         </div>
     )
 }
+/*
+            {model.training
+                && model.training.state==='running'
+                && model.training.progress !== undefined ? (
+                <Progress 
+                    maxEvals={model.training.progress.max_evals}
+                    iteration={model.training.progress.eval} />
+                ) : null}
+                        <Progress 
+                            maxEvals={model.training.progress.max_evals}
+                            eval={model.training.progress.max_evals} />
+                    <progress
+                        value={model.training.progress.max_evals}
+                        max={model.training.progress.max_evals}>
+                    </progress>
+*/
 
 ModelStatus.propTypes = {
     model: PropTypes.shape({}).isRequired,
