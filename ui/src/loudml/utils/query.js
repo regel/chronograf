b274a2180b0684377b7a6eab347b3310beda0239
Samuel Bettevy <samuel.bettevy@gmail.com>
2018-07-19 09:22:54 +0200

    fix model query from measurement(s)

--- ui/src/loudml/utils/query.js
+++ ui/src/loudml/utils/query.js
@@ -12,21 +12,42 @@ import FeaturesUtils from 'src/loudml/components/FeaturesUtils'
  *  time(30m)
  * FILL(null)
  */
-const createQueryFields = (prefix, alias, features, database, groupBy) => {
+const uniqueArray = arrArg => {
+    return arrArg.filter((elem, pos,arr) => {
+        return arr.indexOf(elem) === pos
+    })
+}
+
+const createPrefixedQueryFields = (prefix, model, database) => {
+    const {name, features, bucket_interval} = model
     const fields = features.map(f => `LAST("${prefix}${f.name}")`).join(' + ')
-    return `SELECT ${fields} AS "${prefix}${alias}" FROM "${database}"."autogen"."prediction_${alias}" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(${groupBy}) FILL(null)`
+    return `SELECT ${fields} AS "${prefix}${name}" FROM "${database}"."autogen"."prediction_${name}" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(${bucket_interval}) FILL(null)`
+}
+
+const createQueryFields = (model, database) => {
+    const {name, features, bucket_interval} = model
+
+    const fields = features.map(f => `${f.metric}("${f.field}")`).join(' + ')
+    const from = uniqueArray(features.map(f => `"${database}"."autogen"."${f.measurement}"`)).join(', ')
+    return `SELECT ${fields} AS "${name}" FROM ${from} WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(${bucket_interval}) FILL(null)`
+    // SELECT ${metric}("${field-name}") AS "${metric}-${field-name}" FROM "${database}"."autogen"."${measurement}" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(${bucket_interval}) FILL(null)
 }
 
 export const createQueryFromModel = (model, source, database) => {
-    const {settings: {name, features, bucket_interval}} = model
+    // const {settings: {features}} = model
     const {links: {self}} = source
     
-    const d = FeaturesUtils.deserializedFeatures(features)
+    const features = FeaturesUtils.deserializedFeatures(model.settings.features)
         .filter(feature => feature.io!=='i')
 
+    const m = {
+        ...model.settings,
+        features,
+    }
+
     return [
-        { query: createQueryFields('lower_', name, d, database, bucket_interval), source: self, },
-        { query: createQueryFields('', name, d, database, bucket_interval), source: self, },
-        { query: createQueryFields('upper_', name, d, database, bucket_interval), source: self, },
+        { query: createPrefixedQueryFields('lower_', m, database), source: self, },
+        { query: createQueryFields(m, database), source: self, },
+        { query: createPrefixedQueryFields('upper_', m, database), source: self, },
     ]
 }
