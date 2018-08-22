ca041ea3c271754e5ba5ae8c384e4cdb14ea2577
Samuel Bettevy <samuel.bettevy@gmail.com>
2018-08-22 23:18:27 +0200

    feature params in query: match_all (tags) and default (fill)

--- ui/src/loudml/utils/query.js
+++ ui/src/loudml/utils/query.js
@@ -1,77 +1,17 @@
 import {buildQuery} from 'utils/influxql'
 import {TYPE_QUERY_CONFIG} from 'src/dashboards/constants'
 
-// import buildQueries from 'utils/buildQueriesForGraphs'
 import FeaturesUtils from 'src/loudml/components/FeaturesUtils'
 
-/* SELECT
- *  mean("sin1") + stddev("sin2")
- * AS
- *  "sin"
- * FROM
- *  "ecommerce"."autogen"."prediction_sin"
- * WHERE
- *  time > :dashboardTime:
- * GROUP BY
- *  time(30m)
- * FILL(null)
- */
-/*
-const uniqueArray = arrArg => {
-    return arrArg.filter((elem, pos,arr) => {
-        return arr.indexOf(elem) === pos
-    })
-}
-*/
-/*
-const createPrefixedQueryFields = (prefix, model, database) => {
-    const {name, features} = model
-    const fields = features.map(f => `LAST("${prefix}${f.name}")`).join(' + ')
-    return `SELECT ${fields} AS "${prefix}${name}" FROM "${database}"."autogen"."prediction_${name}" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(${model.bucket_interval}) FILL(null)`
-}
-*/
-/*
-const createQueryFields = (model, database) => {
-    const {name, features} = model
-
-    const fields = features.map(f => `${f.metric}("${f.field}")`).join(' + ')
-    const from = uniqueArray(features.map(f => `"${database}"."autogen"."${f.measurement}"`)).join(', ')
-    return `SELECT ${fields} AS "${name}" FROM ${from} WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(${model.bucket_interval}) FILL(null)`
-    // SELECT ${metric}("${field-name}") AS "${metric}-${field-name}" FROM "${database}"."autogen"."${measurement}" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(${bucketInterval}) FILL(null)
-}
-*/
-
-const QUERY_CONFIG_FIELD_ARG = {
-    "value": "lower_sin1",
-    "type": "field",
-    "alias": "",
-}
-
-const QUERY_CONFIG_FIELD = {
-    "value": "mean",
-    "type": "func",
-    "alias": "mean_lower_sin1",
-    "args": [],
-}
-
 const QUERY_CONFIG = {
-    "database": null,
-    "measurement": null,
-    "retentionPolicy": "autogen",
-    "fields": [],
-    "tags": {},
-    "groupBy": {
-        "time": null,
-        "tags": []
-    },
-    "areTagsAccepted": false,
-    "fill": "null",
-    "rawText": null,
-    "range": {
+    retentionPolicy: 'autogen',
+    areTagsAccepted: true,
+    rawText: null,
+    range: {
         lower: ':dashboardTime:',
         upper: ':upperDashboardTime:'
     },
-    "shifts": null,
+    shifts: null,
 }
 
 const createPrefixedQueryFields = (prefix, model) => {
@@ -79,12 +19,13 @@ const createPrefixedQueryFields = (prefix, model) => {
     return features
         .map(
             f => ({
-                ...QUERY_CONFIG_FIELD,
+                type: 'func',
                 value: (prefix===null?f.metric:'LAST'),
                 alias: (prefix===null?f.name:`${prefix}`),
                 args: [
                     {
-                        ...QUERY_CONFIG_FIELD_ARG,
+                        type: 'field',
+                        alias: '',
                         value: (prefix===null?f.field:`${prefix}_${f.metric}_${f.field}`),
                     }
                 ],
@@ -94,13 +35,20 @@ const createPrefixedQueryFields = (prefix, model) => {
 
 const createQueryConfig = (prefix, model, database) => {
     const {features, name} = model
-    const measurement = (prefix===null?features[0].measurement:`prediction_${name}`)
+    const feature = features[0]
+    const measurement = (prefix===null?feature.measurement:`prediction_${name}`)
+    const tags = feature.match_all
+        .reduce((a, m) => {
+                a[m.tag] = [m.value]
+                return a
+        }, {})
     return {
         ...QUERY_CONFIG,
         fields: createPrefixedQueryFields(prefix, model),
         database,
         measurement,
-        tags: {},   // TODO fill with match_all
+        tags,
+        fill: (prefix===null?feature.default:null),
         groupBy: {
             time: model.bucket_interval,
             tags: [],
@@ -125,19 +73,16 @@ export const createQueryFromModel = (model, source, database) => {
             'lower',
             m,
             database),
-            source: self,
         },
         { queryConfig: createQueryConfig(
             null,
             m,
             database),
-            source: self,
         },
         { queryConfig: createQueryConfig(
             'upper',
             m,
             database),
-            source: self,
         },
     ]
     return [{
@@ -149,34 +94,3 @@ export const createQueryFromModel = (model, source, database) => {
             ).join('; ')
         }]
 }
-
-
-/*
- * SELECT
- *  LAST("lower_mean_foo") AS "lower",
- *  mean("foo") AS "mean_foo",
- *  LAST("upper_mean_foo") AS "upper"
- * FROM 
- *  "metrics"."autogen"."load_avg", 
- *  "metrics"."autogen"."prediction_metrics_load_avg_mean_foo_1m" 
- * WHERE
- *  time > :dashboardTime:
- *  AND time < :upperDashboardTime: 
- * GROUP BY
- *  time(1m) 
- * FILL(null)
- */
-
-/*
-export const createQueryFromModel = (model, source, database) => {
-    const {settings} = model
-    const {links: {self}} = source
-    
-    const features = FeaturesUtils.deserializedFeatures(settings.features)
-        .filter(feature => feature.io!=='i')
-
-    return [{
-        query: "SELECT LAST(\"lower_mean_foo\") AS \"lower\" FROM \"metrics\".\"autogen\".\"prediction_metrics_load_avg_mean_foo_1m\" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(1m) FILL(null); SELECT mean(\"foo\") AS \"mean_foo\" FROM \"metrics\".\"autogen\".\"load_avg\" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(1m) FILL(null); SELECT LAST(\"upper_mean_foo\") AS \"upper\" FROM \"metrics\".\"autogen\".\"prediction_metrics_load_avg_mean_foo_1m\" WHERE time > :dashboardTime: AND time < :upperDashboardTime: GROUP BY time(1m) FILL(null)"
-    }]
-}
-*/
