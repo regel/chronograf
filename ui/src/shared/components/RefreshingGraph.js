5dea0142adde95ada52a407c8af7a304e1cca1d9
Samuel Bettevy <samuel.bettevy@gmail.com>
2018-08-28 23:28:45 +0200

    fix graph legend

--- ui/src/shared/components/RefreshingGraph.js
+++ ui/src/shared/components/RefreshingGraph.js
@@ -135,7 +135,6 @@ const RefreshingGraph = ({
   if (type === 'confidence') {
     return (
       <RefreshingConfidenceGraph
-        colors={colors}
         key={manualRefresh}
         queries={queries}
         templates={templates}
@@ -149,6 +148,8 @@ const RefreshingGraph = ({
         onSetHoverTime={onSetHoverTime}
         inView={inView}
         setDataLabels={setDataLabels}
+        onZoom={onZoom}
+        timeRange={timeRange}
       />
     )
   }
