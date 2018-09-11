c8d943456b569661e998c4b49c250e2769d23bf2
Samuel Bettevy <samuel.bettevy@gmail.com>
2018-09-11 18:53:22 +0200

    delete feature confirm option

--- ui/src/loudml/components/FeatureHeader.js
+++ ui/src/loudml/components/FeatureHeader.js
@@ -1,61 +1,90 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import ConfirmButton from 'shared/components/ConfirmButton'
+import ConfirmOrCancel from 'shared/components/ConfirmOrCancel'
 
 const FeatureHeader = ({
-  feature,
-  onEdit,
-  onKeyDown,
-  onConfirm,
-  // onCancel,
-  onDelete,
-  disabled,
-}) => {
-  if (feature.isEditing) {
-    return (
-      <EditHeader
-        feature={feature}
-        onEdit={onEdit}
-        onKeyDown={onKeyDown}
-        onConfirm={onConfirm}
-        onCancel={onDelete}
-      />
-    )
-  }
-
-  return (
-    <Header
-      feature={feature}
-      onDelete={onDelete}
-      disabled={disabled}
-    />
-  )
-}
-
-const Header = ({
     feature,
+    onEdit,
+    onKeyDown,
+    onConfirm,
     onDelete,
     disabled,
 }) => {
+    if (feature.isEditing) {
+        return (
+            <EditHeader
+                feature={feature}
+                onEdit={onEdit}
+                onKeyDown={onKeyDown}
+                onConfirm={onConfirm}
+                onCancel={onDelete}
+            />
+        )
+    }
 
     return (
-        <div className="db-manager-header">
-            <h4>{feature.name}</h4>
-            <div className="db-manager-header--actions text-right">
-                {disabled
-                ?null
-                :(
-                <ConfirmButton
-                    square={true}
-                    item={feature}
-                    onDelete={onDelete}
-                    // onCancel={onCancel}
-                    buttonSize="btn-xs"
-                />)}
-            </div>
-        </div>
+      <Header
+          feature={feature}
+          onDelete={onDelete}
+          disabled={disabled}
+      />
     )
+}
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isDeleting: false,
+        }
+    }
+
+    onStartDelete = () => {
+        this.setState({isDeleting: true})
+    }
+
+    onCancelDelete = () => {
+      this.setState({isDeleting: false})
+    }
+
+    render() {
+        const {
+            feature,
+            onDelete,
+            disabled,
+        } = this.props
+        const {isDeleting} = this.state
+
+        const buttons = (
+          disabled
+              ?null
+              :(<button
+                className="btn btn-xs btn-danger"
+                onClick={this.onStartDelete}
+                >
+                Delete
+                </button>)
+        )
+          
+        const deleteConfirmation = (
+            <ConfirmOrCancel
+                square={true}
+                item={feature}
+                onConfirm={onDelete}
+                onCancel={this.onCancelDelete}
+                buttonSize="btn-xs"
+            />)
+            
+        return (
+            <div className="db-manager-header">
+                <h4>{feature.name}</h4>
+                <div className="db-manager-header--actions text-right">
+                    {isDeleting?deleteConfirmation:buttons}
+                </div>
+            </div>)
+    }
 }
 
 const EditHeader = ({
@@ -65,33 +94,36 @@ const EditHeader = ({
     onConfirm,
     onCancel
 }) => (
-  <div className="db-manager-header db-manager-header--edit">
-    <input
-      className="form-control input-sm"
-      name="name"
-      type="text"
-      value={feature.name}
-      placeholder="Name this Feature"
-      onChange={onEdit(feature)}
-      onKeyDown={onKeyDown(feature)}
-      autoFocus={true}
-      spellCheck={false}
-      autoComplete={false}
-    />
-    <ConfirmButton item={feature} onConfirm={onConfirm} onCancel={onCancel} />
-  </div>
+    <div className="db-manager-header db-manager-header--edit">
+        <input
+            className="form-control input-sm"
+            name="name"
+            type="text"
+            value={feature.name}
+            placeholder="Name this Feature"
+            onChange={onEdit(feature)}
+            onKeyDown={onKeyDown(feature)}
+            autoFocus={true}
+            spellCheck={false}
+            autoComplete="false"
+        />
+        <ConfirmOrCancel
+            item={feature}
+            onConfirm={onConfirm}
+            onCancel={onCancel} />
+    </div>
 )
 
 const {func, shape, bool} = PropTypes
 
 FeatureHeader.propTypes = {
-  onEdit: func,
-  feature: shape(),
-  onKeyDown: func,
-  // onCancel: func,
-  onDelete: func,
-  onConfirm: func,
-  disabled: bool.isRequired,
+    onEdit: func,
+    feature: shape(),
+    onKeyDown: func,
+    // onCancel: func,
+    onDelete: func,
+    onConfirm: func,
+    disabled: bool.isRequired,
 }
 
 Header.propTypes = {
